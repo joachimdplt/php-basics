6dfe629ca112106e729ba22d8668a13b9a5dba16
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-11 22:36:57 +0100

    first commit

---
+++ Tutos/Affichage.php
@@ -0,0 +1,6 @@
+/*
+echo = afficher du code ou une valeur sur la page
+print = alternative à echo + lente
+$ = déclrarer une variable
+" " = Chaînes de caractères, permettant l’interpolation de variables.
+'' = Chaînes de caractères sans interpolation de variable.
