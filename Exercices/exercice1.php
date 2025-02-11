5e37b20035f02ed227710a9beb26bf911b2b3f53
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-11 23:45:17 +0100

    Afficher un message avec des variables

--- Exercices/exercice1.php
+++ Exercices/exercice1.php
@@ -1,1 +1,23 @@
+<?php
+
 /* 
+
+Exercice 1 : Afficher un message avec des variables
+
+Objectif : Apprendre à stocker des données dans des variables et les afficher.
+
+Énoncé
+
+Crée un script PHP qui stocke ton prénom et ton âge dans des variables et affiche la phrase suivante :
+
+“Je m’appelle [Prénom] et j’ai [Âge] ans.”
+
+*/
+
+$name = "Doe";
+$firstname = "John";
+$age = 20; 
+
+echo "Je m'appelle $firstname $name, et j'ai $age ans";
+
+?>
