71e1317c3025c18a0afbd475b5ade18f9995a958
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-11 23:02:00 +0100

    Conditions et operérateurs en php

---
+++ Tutos/ConditionsOperateurs.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+if	              Exécute un bloc de code si la condition est vraie.
+else	          Exécute un bloc de code si la condition if est fausse.
+elseif	          Vérifie une autre condition si la première est fausse.
+==	              Vérifie l’égalité entre deux valeurs.
+===	              Vérifie l’égalité stricte (même type et même valeur).
+!=	              Vérifie si deux valeurs sont différentes.
+>	              Vérifie si une valeur est supérieure à une autre.
+<	              Vérifie si une valeur est inférieure à une autre.
+*/
+$age = 18;
+if ($age >= 18) {
+    echo "Majeur";
+} else {
+    echo "Mineur";
+}
+
+?>
