2bbda9c7ba7faa98f6ca925ea4257dcd689ac58d
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-11 23:30:12 +0100

    cookies-sessions

---
+++ Tutos/cookies-sessions.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+
+setcookie()	                Crée un cookie stocké sur l’ordinateur de l’utilisateur.
+
+$_COOKIE	                Accède aux cookies stockés.
+
+session_start()	            Démarre une session PHP.
+
+$_SESSION	                Stocke des données de session accessibles sur plusieurs pages
+
+*/
+
+?>
