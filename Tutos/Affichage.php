31b08ab3e08531fdb3e9a63ad584a4d157658ffa
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-02-11 22:54:57 +0100

    regles d'affichages php

--- Tutos/Affichage.php
+++ Tutos/Affichage.php
@@ -1,6 +1,24 @@
+<?php
+
 /*
+
 echo = afficher du code ou une valeur sur la page
+
 print = alternative à echo + lente
+
 $ = déclrarer une variable
+
 " " = Chaînes de caractères, permettant l’interpolation de variables.
+
 '' = Chaînes de caractères sans interpolation de variable.
+
+L’interpolation permet d’insérer directement des variables dans une chaîne de caractères sans utiliser de . pour les concaténer.
+
+La concaténation en PHP permet d’assembler plusieurs morceaux de texte et/ou de variables en une seule chaîne de caractères.
+En PHP, l’opérateur de concaténation est le point (.).
+
+*/
+
+$nom = "Alice";
+echo "Bonjour, $nom"; // ✅ Affiche : Bonjour, Alice
+?>
